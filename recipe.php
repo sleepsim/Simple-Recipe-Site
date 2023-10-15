5d6f9559f14600921b35d35d4b58c538b8a02e95
sleepsim <91400544+sleepsim@users.noreply.github.com>
2023-10-14 17:50:08 -0700

    Update recipe.php

--- recipe.php
+++ recipe.php
@@ -39,8 +39,16 @@
             while($array = fgetcsv($testing24)){
                 echo $array[0] . ",";
                 echo $array[1] . ",";
-                echo $array[2] . "<br>";
+                echo $array[2] . ",";
+                echo $array[3] . ",";
+                echo $array[4] . ",";
+                echo $array[5] . ",";
+                echo $array[6] . ",";
+                echo $array[7] . "<br>";
             }
+
+            
+
 
             divEnd();
             generateFooter();
